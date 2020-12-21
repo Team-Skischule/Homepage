73b100081c7ae3d106a496dc02a85202789f90eb
paul <paul.amann@gmx.at>
2020-12-21 15:21:43 +0100

    footer und header php für includes angelegt

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+      <p>Impressum</p>
+      <p>Inhalt hier einfügen!</p>
+    </footer>
+
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+      crossorigin="anonymous"
+    ></script>
+
+  <!-- Add active class to nav Element when on specific url -->
+    <script type="text/javascript">
+    $(document).ready(function () {
+        var url = window.location;
+        $('ul.navbar-nav a[href="'+ url +'"]').parent().addClass('active');
+        $('ul.navbar-nav a').filter(function() {
+             return this.href == url;
+        }).parent().addClass('active');
+    });
+</script> 
+  </body>
+</html>
