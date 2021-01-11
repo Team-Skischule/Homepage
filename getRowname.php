fcdd9bb62ea377a1c75f1b2b4dbdac6bb7449d5e
Mr_Rudi <kuengi@gmail.com>
2021-01-11 14:40:46 +0100

    s

--- getRowname.php
+++ getRowname.php
@@ -4,6 +4,7 @@
 
     $stmt = $link->prepare("SELECT id, username as name FROM skischule.users");
 
+    
     $stmt->execute();
     $result = $stmt->get_result();
     $outp = $result->fetch_all(MYSQLI_ASSOC);
