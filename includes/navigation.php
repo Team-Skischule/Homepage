8c0d2b4fb4c19fc85d14897411d2f047fbfe733b
paul <paul.amann@gmx.at>
2021-01-12 12:03:56 +0100

    neu erstellt um Navigations-Menü von head inhalten zu trennen

---
+++ includes/navigation.php
@@ -0,0 +1,24 @@
+    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/Homepage/willkommen.php">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Homepage/SkilehrerAnlegen/skilehrer.php">Skilehrer-Übersicht</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Homepage/ajax-abfrage.html">Ajax Abfrage</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/TimeScheduler-master/Calendar.html">Kalender</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
