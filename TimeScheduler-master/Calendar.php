adf02861565c6c2ba452708f8f471f547ac805eb
paul <paul.amann@gmx.at>
2021-01-18 15:36:41 +0100

    Tab mechanismus Bau begonnen. JS funktioniert noch nicht

--- TimeScheduler-master/Calendar.php
+++ TimeScheduler-master/Calendar.php
@@ -31,43 +31,46 @@
                 </div>
             <div class="row">
                 <div class="col-sm-3">
-                    <!-- https://www.youtube.com/watch?v=sn2hRCOR-Qg -->
+                <div class="result">  </div>
                     <div class="tabs">
                         <div class="tab-header">
-                            <div>
-                                <i class="active fa fa-calendar-plus-o" aria-hidden="true"><p>neu</p></i>
-                            </div>
-                            <div >
-                                <i class="fa fa-calendar-minus-o" aria-hidden="true"><p>edit</p></i>
-                            </div>
-                            <div>
-                                <i class="fa fa-user-plus" aria-hidden="true" ><p>neu</p></i>
-                            </div>
-                            <div>
-                                <i class="fa fa-users" aria-hidden="true"><p>edit</p></i>
+                            <!-- https://www.sitepoint.com/community/t/same-tab-after-page-refresh/39684/4 -->
+                            <div class="tab">
+                                <a href="?tab=newDate" name="newDate">
+                                    <i class="tablinks fa fa-calendar-plus-o" onclick="openTab(event, 'newDate')" id="defaultOpen" ><p>neu</p></i>
+                                </a> 
+                                <a href="?tab=editDate" name="editDate">
+                                    <i class="tablinks fa fa-calendar-minus-o" onclick="openTab(event, 'editDate')"href="?tab=editDate"><p>edit</p></i>
+                                </a> 
+                                <a href="?tab=newSkilehrer" name="newSkilehrer">
+                                    <i class="tablinks fa fa-user-plus" onclick="openTab(event, 'newSkilehrer')"><p>neu</p></i>
+                                </a> 
+                                <a href="?tab=editSkilehrer" name="editSkilehrer">
+                                    <i class="tablinks fa fa-users" onclick="openTab(event, 'editSkilehrer')"><p>edit</p></i>
+                                </a> 
                             </div>
                         </div>
-                        <div class="tab-indicator"></div>
                         <div class="tab-content">
-                            <div class="active">
+                            <div id="newDate" class="tabcontent">
                                 <i class="fa fa-calendar-plus-o"></i>
                                 <h2>Neuen Termin anlegen</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                                <?php include '../termine-anlegen-neu.php';?>
                             </div>
-                            <div>
+
+                            <div id="editDate" class="tabcontent">
                                 <i class="fa fa-calendar-minus-o"></i>
                                 <h2>Termin bearbeiten</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
                             </div>
-                            <div>
+
+                            <div id="newSkilehrer" class="tabcontent">
                                 <i class="fa fa-user-plus"></i>
                                 <h2>Mitarbeiter anlegen</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                                <?php include '../skilehrer-anlegen-neu.php';?>
                             </div>
-                            <div>
+
+                            <div id="editSkilehrer" class="tabcontent">
                                 <i class="fa fa-users"></i>
                                 <h2>Mitarbeiter bearbeiten</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
                             </div>
                         </div>
                     </div>
@@ -75,44 +78,84 @@
                
                 <div class="col-sm-9">
                     <div class="content">
-                        <!-- <div>
-                            <a href="/Homepage/termine-anlegen.php"> <button type="button" class="btn btn-outline-primary">Neuen Termin Anlegen</button></a> 
-                            <a href="/Homepage/skilehrer-anlegen.php"> <button type="button" class="btn btn-outline-primary">Neuen Skilehrer Anlegen</button></a> 
-                        </div> -->
                         <div class="calendar">
-            
                         </div>
                         <div class="realtime-info">
-                            
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-<!-- Tab Mechanismus TODO: noch in anderes js.file verschieben -->
+        
+
+        <!-- Tab Mechanismus TODO: noch in anderes js.file verschieben -->
         <script>
-            console.log('wird geladen')
-            function _class(name){
-            return document.getElementsByClassName(name);
+
+            function getParameterByName(name) {
+                name = name.replace(/[\\[]/, "\\\\[").replace(/[\\]]/, "\\\\]");
+                var regex = new RegExp("[\\\\?&]" + name + "=([^&#]*)"),
+                    results = regex.exec(location.search);
+                    return results == null ? "" : decodeURIComponent(results[1].replace(/\\+/g, " "));
+                }
+                
+            var tab = getParameterByName('tab');
+                
+            if(tab) {
+                // open same tab
+                console.log('tab: ' + tab)
+                var y = document.getElementsByName(tab)[0].children;
+                // TODO:
+                console.log('y.value: ' +y[0].classList.value );
+                console.log('y.value: ' +y[0].classList.value );
+                function clickTab() {
+                    document.getElementsByClassName(y[0].classList.value).click()
+                };
+                clickTab();
+            } else {
+                // Get the element with id="defaultOpen" and click on it
+               document.getElementById("defaultOpen").click();
             }
 
-            let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+            /* var activeTab = document.getElementsByClassName("active");
+            if (sessionStorage.getItem("autosave")) {
+                // Den Inhalt des Testfeldes aus dem sessionStorage wiederherstellen
+                activeTab.value = sessionStorage.getItem("autosave");
+                console.log('activeTab: ' + activeTab.value);
+            } else {
+                console.log('smonthing')
+                // Get the element with id="defaultOpen" and click on it
+               document.getElementById("defaultOpen").click();
+            } */
 
-            for(let i=0;i<tabPanes.length;i++){
-            tabPanes[i].addEventListener("click",function(){
-                _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
-                tabPanes[i].classList.add("active");
-                
-                _class("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;
-                console.log("i: " + i)
-                _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
-                _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
-                
-            });
+
+            
+
+            // Click Function
+            function openTab(evt, tabID) {
+                var i, tabcontent, tablinks;
+                // bei klick auf Tab wird dieser als active Tab in sessionStorage gespeichert:
+                /* sessionStorage = document.getElementsByClassName("active");
+                console.log('activeTab: ' + activeTab) */
+
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                document.getElementById(tabID).style.display = "block";
+                evt.currentTarget.className += " active";
             }
         </script>
- 
+        <script src="/Homepage/termine-anlegen-process/CreateTermineDbEntry.js" 
+            crossorigin="anonymous"></script>
+        <script src="/Homepage/termine-anlegen-process/skilehrer-live-search.js" 
+            crossorigin="anonymous"></script>
+      
+    
     </body> 
     
 </html>
