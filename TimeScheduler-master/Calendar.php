9b23ba7d2c759bcc885b5682bdba1876c0b93ead
paul <paul.amann@gmx.at>
2021-01-18 08:33:13 +0100

    Tab mechanismus angepasst

--- TimeScheduler-master/Calendar.php
+++ TimeScheduler-master/Calendar.php
@@ -33,45 +33,57 @@
             </div>
             <div class="row">
                 <div class="col-sm-3">
-                    <!-- https://getbootstrap.com/docs/4.0/components/navs/ -->
-                    <div class="container">
-                    <h2>Dynamic Tabs</h2>
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-                        <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
-                        <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
-                        <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
-                    </ul>
-
-                    <div class="tab-content">
-                        <div id="home" class="tab-pane fade in active">
-                        <h3>HOME</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <!-- https://www.youtube.com/watch?v=sn2hRCOR-Qg -->
+                    <div class="tabs">
+                        <div class="tab-header">
+                            <div class="active">
+                                <i class="far fa-calendar-plus">Termine neu</i>
+                            </div>
+                            <div >
+                                <i class="far fa-calendar-minus">Termine bearbeiten</i>
+                            </div>
+                            <div>
+                                <i class="fa fa-users">Mitarbeiter Neu</i>
+                            </div>
+                            <div>
+                                <i class="fa fa-bar-user-edit">Mitarbeiter bearbeiten</i>
+                            </div>
+                            <div>
+                                <i class="fa fa-bar-envelop">Contact</i>
+                            </div>
                         </div>
-                        <div id="menu1" class="tab-pane fade">
-                        <h3>Menu 1</h3>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                        <div id="menu2" class="tab-pane fade">
-                        <h3>Menu 2</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        </div>
-                        <div id="menu3" class="tab-pane fade">
-                        <h3>Menu 3</h3>
-                        <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        <div class="tab-indicator"></div>
+                        <div class="tab-content">
+                            <div class="active">
+                                <i class="fa fa-code">Code</i>
+                                <h2>this is code section</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                            </div>
+                            <div>
+                                <i class="fa fa-pencil-square">about</i>
+                                <h2>this is about section</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                            </div>
+                            <div>
+                                <i class="fa fa-chart">Service</i>
+                                <h2>this is chart section</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                            </div>
+                            <div>
+                                <i class="fa fa-envelope">Contact</i>
+                                <h2>this is envelop section</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quperiores sint ducimus eum nam natus cupiditate magni quas nemo! Fugit.</p>
+                            </div>
                         </div>
                     </div>
-                    </div>
-
-                    
                 </div>
                
                 <div class="col-sm-9">
                     <div class="content">
-                        <div>
+                        <!-- <div>
                             <a href="/Homepage/termine-anlegen.php"> <button type="button" class="btn btn-outline-primary">Neuen Termin Anlegen</button></a> 
                             <a href="/Homepage/skilehrer-anlegen.php"> <button type="button" class="btn btn-outline-primary">Neuen Skilehrer Anlegen</button></a> 
-                        </div>
+                        </div> -->
                         <div class="calendar">
             
                         </div>
@@ -82,6 +94,29 @@
                 </div>
             </div>
         </div>
+
+<!-- Tab Mechanismus TODO: noch in anderes js.file verschieben -->
+        <script>
+            console.log('wird geladen')
+            function _class(name){
+            return document.getElementsByClassName(name);
+            }
+
+            let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+
+            for(let i=0;i<tabPanes.length;i++){
+            tabPanes[i].addEventListener("click",function(){
+                _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+                tabPanes[i].classList.add("active");
+                
+                _class("tab-indicator")[0].style.top = `calc(80px + ${i*50}px)`;
+                console.log("i: " + i)
+                _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+                _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+                
+            });
+            }
+        </script>
  
     </body> 
     
