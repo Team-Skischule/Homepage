5104bf80b72aa65eb5ad6738de4e290aea33c1c8
paul <paul.amann@gmx.at>
2021-01-18 07:11:24 +0100

    Echo PHP List All Session Variable auskommentiert, da sie das Styling erschweren

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,9 @@
 // Initialize the session
 session_start();
 
-echo "<h3> PHP List All Session Variables</h3>";
+/* echo "<h3> PHP List All Session Variables</h3>";
 foreach ($_SESSION as $key => $val)
-  echo $key . " " . $val . "<br/>";
+  echo $key . " " . $val . "<br/>"; */
 // Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   header("location: /Homepage/login.php");
