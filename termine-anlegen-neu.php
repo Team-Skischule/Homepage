6e872ebaf60efd9d4621c2e0ee4f91a622ebe293
paul <paul.amann@gmx.at>
2021-01-19 09:08:39 +0100

    spaltenbezeichnung col-9 auf col-8 angepasst + css darstellung

--- termine-anlegen-neu.php
+++ termine-anlegen-neu.php
@@ -2,30 +2,30 @@
  <form class="content-container" id="form" action="" name="contact" >
     <div class="result"></div>
     <div class="form-group row search-box">
-        <label for="skilehrername" class="col-sm-3 col-form-label">Skilehrer* </label>
-        <input type="text" value="" class="form-control col-sm-9" name="skilehrername" id="skilehrername" autocomplete="off" placeholder="Suche Skilehrer ..." required>
+        <label for="skilehrername" class="col-sm-4  col-form-label">Skilehrer* </label>
+        <input type="text" value="" class="form-control col-sm-8" name="skilehrername" id="skilehrername" autocomplete="off" placeholder="Suche Skilehrer ..." required>
         <div class="skilehrerResult liveSearchResultStyle"></div>
         <div id="skilehrerid"></div>
     </div>
 
     <div class="form-group row">
-        <label for="abholort" class="col-sm-3 col-form-label">Abholort*</label>
-        <input type="text" class="form-control col-sm-9" name="abholort" id="abholort"minlength="1" required>
+        <label for="abholort" class="col-sm-4  col-form-label">Abholort*</label>
+        <input type="text" class="form-control col-sm-8" name="abholort" id="abholort"minlength="1" required>
     </div>
 
     <div class="form-group row">    
-        <label for="kundenname" class="col-sm-3 col-form-label">Kunde*</label> 
-        <input type="text" name="kundenname" id="kundenname" class="form-control col-sm-9"  required>
+        <label for="kundenname" class="col-sm-4  col-form-label">Kunde*</label> 
+        <input type="text" name="kundenname" id="kundenname" class="form-control col-sm-8"  required>
     </div>
 
     <div class="form-group row">
-        <label for="beginn" class="col-sm-3 col-form-label">Beginn*</label>
-        <input type="date" class="form-control col-sm-9" name="beginn" id="beginn" required>
+        <label for="beginn" class="col-sm-4  col-form-label">Beginn*</label>
+        <input type="date" class="form-control col-sm-8" name="beginn" id="beginn" required>
     </div> 
 
     <div class="form-group row">
-        <label for="ende" class="col-sm-3 col-form-label">Ende*</label>
-        <input type="date" class="form-control col-sm-9" name="ende" id="ende" required>
+        <label for="ende" class="col-sm-4  col-form-label">Ende*</label>
+        <input type="date" class="form-control col-sm-8" name="ende" id="ende" required>
     </div>    
 
     <div class="row">
