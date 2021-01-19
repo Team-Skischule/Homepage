ed7d514aad4307549c3333a9a81fe11c8e0cc724
paul <paul.amann@gmx.at>
2021-01-19 10:03:59 +0100

    tab Versuch wieder entfernt und zurückgestellt

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,41 +1,23 @@
-    
-    <header>
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-        <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-        <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
-    </div>
-    <div class="tab-content" id="v-pills-tabContent">
-        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-    </div>
-
-
-   
-    <!-- ORIGINAL (vor der vertikalen Sidebar Tab Ansicht)
-        <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/Homepage/TimeScheduler-master/Calendar.php">Skilehrer-Übersicht</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/Homepage/skilehrer-anlegen.php">Skilehrer Anlegen </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/Homepage/termine-anlegen.php">Termine Anlegen </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/Homepage/logout.php">logout</a>
-                </li>
-                </ul>
-            </div>
-        </nav>
-    </header> -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/Homepage/TimeScheduler-master/Calendar.php">Skilehrer-Übersicht</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Homepage/skilehrer-anlegen.php">Skilehrer Anlegen </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Homepage/termine-anlegen.php">Termine Anlegen </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Homepage/logout.php">logout</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+</header> 
