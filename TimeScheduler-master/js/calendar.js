f9d52f915cb23a8a508309cb11b66240539faa40
paul <paul.amann@gmx.at>
2021-01-20 08:40:02 +0100

    modal box close function zugefügt

--- TimeScheduler-master/js/calendar.js
+++ TimeScheduler-master/js/calendar.js
@@ -124,6 +124,14 @@ var Calendar = {
       ' <br>Start: ' + item.start + 
       ' <br>Ende: ' + item.end;
   
+      // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
   },
 
   Item_Dragged: function (item, sectionID, start, end) {
@@ -270,21 +278,3 @@ function getItemsTest() {
 getItemsTest();
 
 $(document).ready(Calendar.Init);
-
-
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
