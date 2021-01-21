f314262dc7f3ffd102899a26072200d823a2142f
paul <paul.amann@gmx.at>
2021-01-21 13:52:32 +0100

    aufgeräumt console logs entfernt

--- termine-anlegen-process/validate-live-date.js
+++ termine-anlegen-process/validate-live-date.js
@@ -12,22 +12,19 @@ $(document).ready(function(){
 
         if(inputVal.length){ 
             $.get("/Homepage/termine-anlegen-process/validate-date.php", {date: inputVal, id: skilehrerid}).done(function(data){
-                // Display the returned data in browser
+                
+                // Display the returned data in browser: Termin besetzt oder noch frei
                 if (data.length > 0) {    
-                    // es gibt hier kein Json sonder nur eine Info: Termin besetzt oder noch frei
                     console.log('data 2: ' + data);
-
-                   
+                    resultDropdown.html('if: ' + data);
                 } else {
+                    resultDropdown.html('else: ' + data);
                    document.getElementsByClassName('skilehrerResult')[0].innerHTML = "<p>Kein Skilehrer mit diesen Anfangsbuchstaben</p>" ;
                 }
-                
-                // Hier die Ausgabe anpassen, auf was immer da auch rein passt.
                 resultDropdown.html(data);
             });
         } else {
             resultDropdown.empty();
         }
     });
-    
 });
