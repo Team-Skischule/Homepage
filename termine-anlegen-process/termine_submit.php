19059fd028844de2e4cabcf23fddec899bb1683b
paul <paul.amann@gmx.at>
2021-01-22 10:05:46 +0100

    Successmessage mit Kundenname, aufgeräumt (console.logs entfernt usw.)

--- termine-anlegen-process/termine_submit.php
+++ termine-anlegen-process/termine_submit.php
@@ -10,7 +10,6 @@ function test_input($data) {
 // define variables and set to empty values
 $validationFailed = false;
 $errors = array();
-//$skilehrerid = $abholort = $kundenname = $beginn = $ende = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -35,14 +34,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $kundenname = test_input($_POST["kundenname"]);
     }
     
-   /*  if (empty($_POST["skilehrerName"])) {
-        $errors["skilehrerName"] = "skilehrerName ist erforderlich";
-        $validationFailed = true;
-    } else {
-        $skilehrerName = test_input($_Post["skilehrerName"]);
-    } */
-
-
     if (empty($_POST["datumBeginn"])) {
         $errors["datumBeginn"] = "datumBeginn ist erforderlich";
         $validationFailed = true;
@@ -62,58 +53,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($validationFailed){
         foreach($errors as $x => $value) {
             $msg =  $x . " = " . $value;
-            echo ("");
-            echo nl2br ($msg."\n");
             error_log(date("Y-F-j, G:i").": in CreateTerminebEntry.php: ".$msg."\n", 3,  "errors-log.log"); 
         }
-    } // Validation OK
-    else {
+    } else {
 
         include "../config.php";
         
-        /* -- Check if E-Mail or Mobile-Telefonnummer already exists in database:
-            1. get all E-Mail from database
-            2. check if entered email is in it
-                2.a if yes return error
-                2.b if no safe to database and return angelegt
-        */
-        
-        // 1. // TODO: Hier müssen die Beginn & End Daten mit der Datenbank abgeglichen werden
-       /*  $emailInDB = "SELECT email, mobile From skilehrer where (email='$email' or mobile='$mobile');";
-        $result = mysqli_query($link,$emailInDB);  */
-        
-        // 2a.    
-        // TODO: Hier muss eine Fehlermeldung kommen wenn sich das Datum überschneidet
-       /*  if(mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            if ($email==$row['email']) {
-                $errorMail = "<li>E-Mail: " . $row['email'] . " wurde bereits verwendet!". "</li>";
-                echo $errorMail; 
-            }
-            if ($mobile==$row['mobile']) {
-                $errorMobile = "<li>Mobile: " . $row['mobile'] . " wurde bereits verwendet!</li>" ;
-                echo $errorMobile;
-            } 
-        } */
-
-        // 2.b
-        /* else { */
         //insert into database
-                $stmt = $link->prepare("
-                    INSERT INTO skischule.termine (skilehrerid, abholort, kundenname, datumBeginn, datumEnde)
-                    VALUES (?,?,?,?,?)");
+        $stmt = $link->prepare("
+            INSERT INTO skischule.termine (skilehrerid, abholort, kundenname, datumBeginn, datumEnde)
+            VALUES (?,?,?,?,?)");
 
-                $stmt->bind_param("issss", $skilehrerid, $abholort, $kundenname, $datumBeginn, $datumEnde);
-                $stmt->execute();
-                echo $stmt->error;
-                echo "Termin für wurde angelegt";
-                error_log(date("Y-F-j, G:i").": in CreateTerminebEntry.php: ".$stmt->error."\n", 3,  "errors-log.log");
-                    
-                $stmt->close();
-                $link->close();
-                
-            /* }  */
+        $stmt->bind_param("issss", $skilehrerid, $abholort, $kundenname, $datumBeginn, $datumEnde);
+        $stmt->execute();
+        echo $stmt->error;
+        echo "Termin für Kunde \"" . $kundenname . "\" wurde angelegt.";
+            
+        $stmt->close();
+        $link->close();
     }  
 }
-
 ?>
