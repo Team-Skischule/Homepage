5ba45983032b5ed0b6fa44719e10a65fb8cbe5a3
paul <paul.amann@gmx.at>
2021-01-25 14:56:52 +0100

    error-log wird nun im Home gespeichert

--- skilehrer-anlegen-process/skilehrer_submit.php
+++ skilehrer-anlegen-process/skilehrer_submit.php
@@ -84,7 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $msg =  $x . " = " . $value;
             echo ("");
             echo nl2br ($msg."\n");
-            error_log(date("Y-F-j, G:i").": in CreateSkilehrerDbEntry.php: ".$msg."\n", 3,  "errors-log.log"); 
+            error_log(date("Y-F-j, G:i").": in CreateSkilehrerDbEntry.php: ".$msg."\n", 3,  "/Homepage/errors-log.log"); 
         }
     } // Validation OK
     else {
