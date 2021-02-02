7bceda7c49277554f0573698f9ce607d54675c5b
Mr_Rudi <kuengi@gmail.com>
2021-02-02 14:50:12 +0100

    graphisch anpassung termin anlegen

--- termine-anlegen-neu.php
+++ termine-anlegen-neu.php
@@ -1,58 +1,58 @@
  <div>
-    <form class="content-container" id="form" action="" name="contact" >
-        <h2>Termin anlegen</h2>
-        <div class="result"></div>
-        <div class="form-group row search-box">
-            <label for="skilehrername" class="col-sm-4  col-form-label">Skilehrer* </label>
-            <div class="col-sm-8 specialFieldFullWidth">
-                <input type="text" value="" class="form-control" name="skilehrername" id="skilehrername" autocomplete="off" placeholder="Suche Skilehrer ..." required>
-                <div class="skilehrerResult liveSearchResultStyle"></div>
-                <div id="skilehrerid"></div>
-            </div>
-        </div>
+     <form class="content-container" id="form" action="" name="contact">
+         <h2>Termin anlegen</h2>
+         <div class="result"></div>
+         <div class="form-group row search-box">
+             <label for="skilehrername" class="col-sm-4  col-form-label">Skilehrer* </label>
+             <div class="col-sm-8 specialFieldFullWidth">
+                 <input type="text" value="" class="form-control" name="skilehrername" id="skilehrername" autocomplete="off" placeholder="Suche Skilehrer ..." required>
+                 <div class="skilehrerResult liveSearchResultStyle"></div>
+                 <div id="skilehrerid"></div>
+             </div>
+         </div>
 
-        <div class="form-group row">
-            <label for="abholort" class="col-sm-4  col-form-label">Abholort*</label>
-            <input type="text" class="form-control col-sm-8" name="abholort" id="abholort"minlength="1" required>
-        </div>
+         <div class="form-group row">
+             <label for="kundenname" class="col-sm-4  col-form-label">Kunde*</label>
+             <input type="text" name="kundenname" id="kundenname" class="form-control col-sm-8" required>
+         </div>
 
-        <div class="form-group row">    
-            <label for="kundenname" class="col-sm-4  col-form-label">Kunde*</label> 
-            <input type="text" name="kundenname" id="kundenname" class="form-control col-sm-8"  required>
-        </div>
+         <div class="form-group row">
+             <label for="abholort" class="col-sm-4  col-form-label">Abholort*</label>
+             <input type="text" class="form-control col-sm-8" name="abholort" id="abholort" minlength="1" required>
+         </div>
 
-        <div class="form-group row">
-            <label for="beginn" class="col-sm-4  col-form-label">Beginn*</label>
-            <div class="col-sm-8 specialFieldFullWidth">
-                <input type="date" class="form-control" name="beginn" id="beginn"  required>
-                <div id="livesearch"></div>
-            </div> 
-        </div> 
-        
-        <div class="form-group row">
-            <label for="ende" class="col-sm-4  col-form-label">Ende*</label>
-            <div class="col-sm-8 specialFieldFullWidth">
-                <input type="date" class="form-control" name="ende" id="ende" required>
-                <div id="livesearch"></div>
-            </div>    
-        </div>    
+         <div class="form-group row">
+             <label for="beginn" class="col-sm-4  col-form-label">Beginn*</label>
+             <div class="col-sm-8 specialFieldFullWidth">
+                 <input type="date" class="form-control" name="beginn" id="beginn" required>
+                 <div id="livesearch"></div>
+             </div>
+         </div>
 
-        <div class="row">
-            <button type="submit" name="submit" class="btn btn-primary"  id="submit_btn" value="Send">Speichern</button>
-        </div>
-    </form>
+         <div class="form-group row">
+             <label for="ende" class="col-sm-4  col-form-label">Ende*</label>
+             <div class="col-sm-8 specialFieldFullWidth">
+                 <input type="date" class="form-control" name="ende" id="ende" required>
+                 <div id="livesearch"></div>
+             </div>
+         </div>
 
-    <reference path=/Homepage/TimeScheduler-master/js/calendar.js" />
+         <div class="row">
+             <button type="submit" name="submit" class="btn btn-primary" id="submit_btn" value="Send">Speichern</button>
+         </div>
+     </form>
 
-    <script>
-        /* mit Klick auf "Speichern"-Button werden die Kalender Items neu geladen */
-        document.getElementById("submit_btn").addEventListener("click", reloadCalenderItems);
+     <reference path=/Homepage/TimeScheduler-master/js/calendar.js" />
 
-        function reloadCalenderItems() {
-            setTimeout(() => {
-                console.log('reload aufgerufen')
-                getItemsTest();  
-            }, 1000);
-        }
-    </script>
-</div>
+     <script>
+         /* mit Klick auf "Speichern"-Button werden die Kalender Items neu geladen */
+         document.getElementById("submit_btn").addEventListener("click", reloadCalenderItems);
+
+         function reloadCalenderItems() {
+             setTimeout(() => {
+                 console.log('reload aufgerufen')
+                 getItemsTest();
+             }, 1000);
+         }
+     </script>
+ </div>
